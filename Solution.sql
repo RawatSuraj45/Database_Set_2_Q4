11b590e2d88477bf92f3e5d002af0371aaa46e4f
RawatSuraj45 <156521323+RawatSuraj45@users.noreply.github.com>
2024-08-03 13:36:32 +0530

    Update Solution.sql

--- Solution.sql
+++ Solution.sql
@@ -1,1 +1,14 @@
+CREATE DATABASE STUDENT_DETAILS;
+USE STUDENT_DETAILS;
+
+CREATE TABLE Student(
+  Addmision_no int,
+  Firstname varchar(25),
+  Surname Varchar(25),
+  DoB Date ,
+  Gender Varchar(25),
+  Year int,
+  Class varchar(20)
+);
+
 SELECT Class , Gender , Count(Class) , Count(Gender) FROM TABLE GROUP BY Count(Class) , Count(Gender) ORDER BY Class , Gender ; 
